be195e54a6ed04982e22f5860cc222444ac2e92d
heistifeh <teefeh26@gmail.com>
2025-04-13 15:27:59 +0100

    no display

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import AppNavbar from "./components/AppNavbar.jsx";
-
+import AppTheme from "../../shared-theme/AppTheme.jsx";
 export default function Dashboard() {
+  const xThemeComponents = {
+    ...chartsCustomizations,
+    ...dataGridCustomizations,
+    ...datePickersCustomizations,
+    ...treeViewCustomizations,
+  };
   return (
     <>
-      <AppNavbar />
-      <h1>Dashboard Route Loaded</h1>
-      <p>This confirms the route is working!</p>
+      <AppTheme {...props} themeComponents={xThemeComponents}>
+        <AppNavbar />
+        <h1>Dashboard Route Loaded</h1>
+        <p>This confirms the route is working!</p>
+      </AppTheme>
     </>
   );
 }
