676ccc734faa5afbf90d46e6706ca4ddfc80f6f7
heistifeh <teefeh26@gmail.com>
2025-04-18 19:57:14 +0100

    token from body

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -3,16 +3,25 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   //get token from cookies
-  const token = req.cookies.token;
-  if (!token) return next(errorHandler(401, "You are not authenticated because no token"));
+  const { token } = req.body;
+
+  if (!token)
+    return next(
+      errorHandler(401, "You are not authenticated because no token")
+    );
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(401, "You are not authenticated because verification failed"));
+    if (err)
+      return next(
+        errorHandler(
+          401,
+          "You are not authenticated because verification failed"
+        )
+      );
     req.user = user;
     next();
   });
 };
-
 
 // basically just collect token and verify, then pass the user to the next middleware
 //two guard clauses, if no token and if there is error while jwt.verify.
