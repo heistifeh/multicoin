aa1c6f7bbbadb1c15b1f057ca4fee4e23b2978bd
heistifeh <teefeh26@gmail.com>
2025-04-18 20:02:18 +0100

    token in model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,55 +1,59 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     username: {
-        type: String,
+      type: String,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastLogin: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
     isVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-    isIdVerified:{
-        type: Boolean,
-        default: false,
+    isIdVerified: {
+      type: Boolean,
+      default: false,
     },
+    token: String,
     resetPasswordToken: String,
     resetPasswordExpiresAt: Date,
     verificationToken: String,
     verificationTokenExpiresAt: Date,
     deposit: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     total_profit: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     total_balance: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     last_load_time: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
