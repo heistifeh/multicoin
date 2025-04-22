a96d7575bc151bac3c9daab8bde28bb5238ad3e4
heistifeh <teefeh26@gmail.com>
2025-04-22 14:32:23 +0100

    caps

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    LastName: {
+    lastName: {
       type: String,
       required: true,
       unique: true,
